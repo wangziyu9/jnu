18d94c97e894f27c56392be52c2dc83a308eb9c5
yur <k1wangziyu@gmail.com>
2019-12-07 23:44:44 +0800

    coding

---
+++ DataStructures/binaryTree.c
@@ -0,0 +1,14 @@
+# include<stdlib.h>
+# include<stdio.h>
+
+typedef struct BiTNode
+{
+    int data;
+    struct binaryTree *lchild;
+    struct binaryTree *rchild;
+}BiTNode, *BiTree;
+
+int main()
+{
+
+}
