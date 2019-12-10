40bc0c760f6bc09111599c188945e4c4d6899b4b
yur <k1wangziyu@gmail.com>
2019-12-10 23:04:38 +0800

    binary tree width and depth

--- 2015/2015_4_full_binary_tree.c
+++ 2015/2015_4_full_binary_tree.c
@@ -8,8 +8,8 @@ typedef struct binaryTree
     struct binaryTree *rchild;
 }BiTNode;
 
-int width[100]= {0,};
 int depth = 0;
+int isFullBT = 1;
 
 BiTNode* insert(BiTNode *subroot, int d)
 {    
@@ -38,11 +38,25 @@ void pret(BiTNode *root, int d)
     if(root)
     {
         // printf("depth %d ", d);
-        width[d] += 1;
         // printf("%d\n",root->data);
         d = d + 1;
         pret(root->lchild, d);
         pret(root->rchild, d);
+    }
+    else
+    {
+        if(depth == 0)
+        {
+            depth = d;
+        }
+        else
+        {
+            if(depth != d)
+            {
+                isFullBT = 0;
+            }
+        }
+        
     }
 }
 
@@ -61,22 +75,23 @@ int main()
     }
     pret(root,0);
 
-    int max = 0;
-    for(int i = 0; i < 100; i++)
+    if(isFullBT)
     {
-        if(width[i] > max)
-            max = width[i];
+        printf("is full binary tree");
     }
-    printf("%d", max);
+    else
+    {
+        printf("is NOT full binary tree");
+    }
     
     return 0;
 }
 
 // 1 -1
-// 1
+// is full binary tree
 
 // 8 9 7 5 7 -1
-// 2
+// is NOT full binary tree
 
 // 8 9 7 5 7 8 9 -1
-// 4
+// is full binary tree
