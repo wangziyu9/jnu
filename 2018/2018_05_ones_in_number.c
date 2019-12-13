497b319cd78e6863e8e9ab0d9a1062ad0f03ccd3
yur <k1wangziyu@gmail.com>
2019-12-13 14:51:18 +0800

    coding

--- 2018/2018_05_ones_in_number.c
+++ 2018/2018_05_ones_in_number.c
@@ -1,13 +1,22 @@
 // 20xx#x
 // Description:
 
-// Define a function
-// Calculate the sum of the sequence 1 - 1/2 + 1/3 - 1/4 ...... 1/n
-// Call functions in Main function
+// 
 
 // Code:
 
 #include<stdio.h>
+#include<stdlib.h>
+
+char *my_itoa(int num, char *str)
+{
+        if(str == NULL)
+        {
+            return NULL;
+        }
+        sprintf(str, "%d", num);
+        return str;
+}
 
 int calc_len(int num)
 {
@@ -19,18 +28,34 @@ int calc_len(int num)
     }
     return l;
 }
-int count_ones(int num)
-{
-    int ones = 0;
-    int n = 1;
 
+// 遍历数字，数字转化为字符串，实现统计 1 的数量
+int traversal_count_ones(int num)
+{
+    char str[16] = {0};
+    int ones = 0;
+    for(int i = 0; i <= num; i++)
+    {
+        my_itoa(i, str);
+        for(int j = 0; j < 16; j++)
+        {
+            if(str[j] == '1')
+                ones++;
+        }
+    }
+    
     return ones;
 }
+
 int main()
 {
-    printf("%d", calc_len(0));
-    printf("%d", count_ones(10));
-
+    int num = 0;
+    while(1)
+    {
+        scanf("%d", &num);
+        printf("the length is:\t%d\n", calc_len(num));
+        printf("count of 1 is:\t%d\n", traversal_count_ones(num));
+    }
 }
 
 
